4ca72dfb575457433d6d05a9c4a116b33948fee1
juliatmuller <julia_t_muller@estudante.sesisenai.org.br>
2026-08-25 09:54:59 -0300

    add- php de cadastrar clientes

---
+++ public/cadastrar_clientes.php
@@ -0,0 +1,72 @@
+<?php 
+include "../infra/conexao.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+<meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Cadastrar Clientes</title>
+
+</head>
+
+<body>
+    <header>
+        <h1> CRUD - Clientes </h1>
+    </header>
+
+<main>
+    <?php if (isset($_GET["sucesso"])) { ?>
+
+    <h2>Cliente cadastrado com sucesso!</h2>
+
+    <a href="../index.php">
+        <button type="button">Voltar para o início</button>
+    </a>
+
+<?php } else { ?>
+
+<h2> Cadastrar Cliente</h2>
+
+    <form action="cadastrar.php" method="POST">
+
+        <input type="hidden" name="tipo" value="usuario">
+
+        <label for="nome">Nome:</label>
+    <input type="text"  name="nome" id="nome" required >
+    <br>
+    <br>
+
+       <label for="email">Email:</label>
+    <input type="text"  name="Email" id="Email" required >
+    <br>
+    <br>
+
+        <label for="telefone">Telefone:</label>
+    <input type="tel"  name="telefone" id="telefone" placeholder="(00) 00000-0000" required >
+    <br>
+    <br>
+
+
+     <label for="endereco">Endereço:</label>
+    <input type="text"  name="endereco" id="endereco" required >
+    <br>
+
+      </form>
+
+    <br>
+    <br>
+
+
+    <a href="../index.php">  <button type="button"> Voltar para o início</button> </a>
+
+<?php } ?>
+
+</main>
+</body>
+</html>
